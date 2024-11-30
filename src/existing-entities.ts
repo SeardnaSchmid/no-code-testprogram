bc5517122bd7c6d28f80684d8ed087dbd8ec823e
SeardnaSchmid <as@as.as>
2024-11-30 19:53:04 +0100

    Enhance README with proof of concept features and build instructions; update CrossSection UUID reference to imported ID

---
+++ src/existing-entities.ts
@@ -0,0 +1,6 @@
+// existing-entities.ts
+
+export class ImportedExistingCrossSectionResult {
+    public static id: string = "uuid.result.crossSection";
+    // Other legacy properties and methods can be added here
+}
